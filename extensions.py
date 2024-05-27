f862fcdc43ed8f0b215261edc0cb385240df4c58
Mahesh Khadodara <mahevad@gmail.com>
2024-05-27 09:53:22 +0530

    added admin reset password

--- extensions.py
+++ extensions.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
 from authlib.integrations.flask_client import OAuth
-from flask_migrate import Migrate
+
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -11,4 +11,3 @@ login_manager = LoginManager()
 csrf = CSRFProtect()
 mail = Mail()
 oauth = OAuth()
-migrate = Migrate()
